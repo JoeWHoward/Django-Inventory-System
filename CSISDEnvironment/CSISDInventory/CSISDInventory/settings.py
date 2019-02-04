0e2c82060ead0c606a98de4f5c5e982ccb1919ca
Joe Howard <JoeWHoward@users.noreply.github.com>
2019-02-04 16:37:26 -0600

    Update settings.py

--- CSISDEnvironment/CSISDInventory/CSISDInventory/settings.py
+++ CSISDEnvironment/CSISDInventory/CSISDInventory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g5ohd4o^j$#%ywenmo6na0q(a-i565mpwo5_o@@+auhp2&eisa'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
